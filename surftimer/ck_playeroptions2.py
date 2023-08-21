f469cf613e9c10a9041ccc0065c8ecf404a3cedf
tslashd <tsbarzev@gmail.com>
2023-08-21 22:43:24 +0300

    put;caching funcs

--- surftimer/ck_playeroptions2.py
+++ surftimer/ck_playeroptions2.py
@@ -1,12 +1,51 @@
 from fastapi import APIRouter, Request, Response, status
 from fastapi.responses import JSONResponse
 from sql import selectQuery, insertQuery
-from globals import redis_client, config, append_request_log, PlayerOptions
-import time, json
-import surftimer.queries
+from globals import append_request_log, get_cache, set_cache
+from pydantic import BaseModel
+import time, json, surftimer.queries
+
 
 router = APIRouter()
 
+
+class PlayerOptions(BaseModel):
+    timer: int
+    hide: int
+    sounds: int
+    chat: int
+    viewmodel: int
+    autobhop: int
+    checkpoints: int
+    gradient: int
+    speedmode: int
+    centrespeed: int
+    centrehud: int
+    teleside: int
+    module1c: int
+    module2c: int
+    module3c: int
+    module4c: int
+    module5c: int
+    module6c: int
+    sidehud: int
+    module1s: int
+    module2s: int
+    module3s: int
+    module4s: int
+    module5s: int
+    prestrafe: int
+    cpmessages: int
+    wrcpmessages: int
+    hints: int
+    csd_update_rate: int
+    csd_pos_x: int
+    csd_pos_y: int
+    csd_r: int
+    csd_g: int
+    csd_b: int
+    prespeedmode: int
+    steamid32: str
 
 
 # ck_playeroptions2
@@ -49,13 +88,10 @@ def selectPlayerOptions(request: Request, response: Response, steamid32: str):
     append_request_log(request)
 
     # Check if data is cached in Redis
-    cached_data = redis_client.get(f"selectPlayerOptions_{steamid32}")
-    if cached_data:
-        # Return cached data
-        # print(json.loads(cached_data))
-        print(
-            f"[Redis] Loaded 'selectPlayerOptions_{steamid32}' ({time.perf_counter() - tic:0.4f}s)"
-        )
+    cache_key = f"selectPlayerOptions_{steamid32}"
+    cached_data = get_cache(cache_key)
+    if cached_data is not None:
+        print(f"[Redis] Loaded '{cache_key}' ({time.perf_counter() - tic:0.4f}s)")
         return JSONResponse(
             status_code=status.HTTP_200_OK, content=json.loads(cached_data)
         )
@@ -65,19 +101,16 @@ def selectPlayerOptions(request: Request, response: Response, steamid32: str):
     if xquery:
         xquery = xquery.pop()
     else:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        xquery = {"steamid32": steamid32}
+        return JSONResponse(
+            status_code=status.HTTP_404_NOT_FOUND, content=json.loads(cached_data)
+        )
 
     # Cache the data in Redis
-    redis_client.set(
-        f"selectPlayerOptions_{steamid32}",
-        json.dumps(xquery),
-        ex=config["REDIS"]["EXPIRY"],
-    )
+    set_cache(cache_key, xquery)
 
     toc = time.perf_counter()
-    xquery["xtime"] = toc - tic
     print(f"Execution time {toc - tic:0.4f}")
+
     return xquery
 
 
@@ -86,46 +119,7 @@ def selectPlayerOptions(request: Request, response: Response, steamid32: str):
     name="Update Player Options",
     tags=["SurfTimer", "ck_playeroptions2"],
 )
-def updatePlayerOptions(
-    request: Request,
-    response: Response,
-    timer: int,
-    hide: int,
-    sounds: int,
-    chat: int,
-    viewmodel: int,
-    autobhop: int,
-    checkpoints: int,
-    gradient: int,
-    speedmode: int,
-    centrespeed: int,
-    centrehud: int,
-    teleside: int,
-    module1c: int,
-    module2c: int,
-    module3c: int,
-    module4c: int,
-    module5c: int,
-    module6c: int,
-    sidehud: int,
-    module1s: int,
-    module2s: int,
-    module3s: int,
-    module4s: int,
-    module5s: int,
-    prestrafe: int,
-    cpmessages: int,
-    wrcpmessages: int,
-    hints: int,
-    csd_update_rate: int,
-    csd_pos_x: int,
-    csd_pos_y: int,
-    csd_r: int,
-    csd_g: int,
-    csd_b: int,
-    prespeedmode: int,
-    steamid32: str,
-):
+def updatePlayerOptions(request: Request, response: Response, data: PlayerOptions):
     """```c
     char[] sql_updatePlayerOptions = ....
     ```"""
@@ -134,49 +128,49 @@ def updatePlayerOptions(
 
     xquery = insertQuery(
         surftimer.queries.sql_updatePlayerOptions.format(
-            timer,
-            hide,
-            sounds,
-            chat,
-            viewmodel,
-            autobhop,
-            checkpoints,
-            gradient,
-            speedmode,
-            centrespeed,
-            centrehud,
-            teleside,
-            module1c,
-            module2c,
-            module3c,
-            module4c,
-            module5c,
-            module6c,
-            sidehud,
-            module1s,
-            module2s,
-            module3s,
-            module4s,
-            module5s,
-            prestrafe,
-            cpmessages,
-            wrcpmessages,
-            hints,
-            csd_update_rate,
-            csd_pos_x,
-            csd_pos_y,
-            csd_r,
-            csd_g,
-            csd_b,
-            prespeedmode,
-            steamid32,
+            data.timer,
+            data.hide,
+            data.sounds,
+            data.chat,
+            data.viewmodel,
+            data.autobhop,
+            data.checkpoints,
+            data.gradient,
+            data.speedmode,
+            data.centrespeed,
+            data.centrehud,
+            data.teleside,
+            data.module1c,
+            data.module2c,
+            data.module3c,
+            data.module4c,
+            data.module5c,
+            data.module6c,
+            data.sidehud,
+            data.module1s,
+            data.module2s,
+            data.module3s,
+            data.module4s,
+            data.module5s,
+            data.prestrafe,
+            data.cpmessages,
+            data.wrcpmessages,
+            data.hints,
+            data.csd_update_rate,
+            data.csd_pos_x,
+            data.csd_pos_y,
+            data.csd_r,
+            data.csd_g,
+            data.csd_b,
+            data.prespeedmode,
+            data.steamid32,
         )
     )
 
     if xquery < 1:
         return JSONResponse(
             status_code=status.HTTP_204_NO_CONTENT,
-            content={"inserted": xquery, "xtime": time.perf_counter() - tic},
+            content={"updated": xquery, "xtime": time.perf_counter() - tic},
         )
 
     # Prepare the response
@@ -184,5 +178,4 @@ def updatePlayerOptions(
     print(f"Execution time {toc - tic:0.4f}")
     # output = ResponseInsertQuery(xquery)
 
-    return {"inserted": xquery, "xtime": time.perf_counter() - tic}
-
+    return {"updated": xquery, "xtime": time.perf_counter() - tic}
